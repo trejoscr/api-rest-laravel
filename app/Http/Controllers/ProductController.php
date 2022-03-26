2d5737ab8debe03fda4f6f6806010c4a49e708d7
Andres MT <amtre93@gmail.com>
2022-03-26 15:59:15 -0600

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $products = Product::all();
+    return $products;
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+      //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $product = new Product();
+    $product->name  = $request->name;
+    $product->price = $request->price;
+    $product->stock = $request->stock;
+    $product->save();
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+      //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+      //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request)
+  {
+    $product = Product::findOrFail($request->id);
+    $product->name  = $request->name;
+    $product->price = $request->price;
+    $product->stock = $request->stock;
+    $product->save();
+    return $product;
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Request $request)
+  {
+    $product = Product::destroy($request->id);
+    return $product;
+  }
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// show all register
+Route::get('/products', [ProductController::class, 'index']);
+
+// create new register
+Route::post('/products', [ProductController::class, 'store']);
+
+
+// update a register
+Route::put('/products/{id}', [ProductController::class, 'update']);
+
+// delete a register
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
